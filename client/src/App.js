1857b2a1026898be4313f4bfa14aa6a9c960716a
bsolis6878 <bsolis6878@gmail.com>
2022-12-15 12:29:22 -0600

    implemented apollo client and login/add user mutations

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,7 @@
 import { useState } from 'react';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
 import Header from './components/Header';
 import Home from './components/Home';
 import Blogs from './components/Blogs';
@@ -6,12 +9,31 @@ import Profile from './components/Profile';
 import Login from './components/Login';
 import Signup from './components/Signup';
 
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
 
   const [currentlySelected, setCurrentlySelected] = useState('Home');
 
   return (
-    <>
+    <ApolloProvider client={client}>
       <Header
         currentlySelected={currentlySelected}
         setCurrentlySelected={setCurrentlySelected}
@@ -21,7 +43,7 @@ function App() {
       {currentlySelected === 'Profile' && <Profile />}
       {currentlySelected === 'Login' && <Login />}
       {currentlySelected === 'Signup' && <Signup />}
-    </>
+    </ApolloProvider>
   );
 }
 
