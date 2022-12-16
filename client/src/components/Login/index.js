7dc98319ab9846d094fbf50cb946a71341b8ea39
bsolis6878 <bsolis6878@gmail.com>
2022-12-16 11:58:18 -0600

    added state/form submit for login

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,16 +1,71 @@
+import { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../../utils/mutations';
+import Auth from '../../utils/auth';
+
 const Login = () => {
+    const [formState, setFormState] = useState({ email: '', password: '' });
+
+    const [login, { error }] = useMutation(LOGIN_USER);
+  
+    // update state based on form input changes
+    const handleChange = (event) => {
+      const { name, value } = event.target;
+  
+      setFormState({
+        ...formState,
+        [name]: value,
+      });
+    };
+  
+    // submit form
+    const handleFormSubmit = async (event) => {
+      event.preventDefault();
+  
+      try {
+        const { data } = await login({
+          variables: { ...formState }
+        });
+  
+        Auth.login(data.login.token);
+      } catch (e) {
+        console.error(e);
+      }
+  
+      // clear form values
+      setFormState({
+        email: '',
+        password: '',
+      });
+    };
+
     return (
         <div className='login-signup'>
             <p>Login to continue.</p>
-            <form>
-                <label for='username'>Username</label>
-                <input id='username' placeholder='Enter your username here'></input>
-                <label for='password'>Password</label>
-                <input id='password' placeholder='Enter your password here'></input>
+            <form onSubmit={handleFormSubmit}>
+                <label htmlFor='email'>Email</label>
+                <input 
+                    id='email'
+                    placeholder='Enter your email here'
+                    type='email'
+                    name='email'
+                    value={formState.email}
+                    onChange={handleChange}
+                />
+                <label htmlFor='password'>Password</label>
+                <input 
+                    id='password'
+                    placeholder='Enter your password here'
+                    name='password'
+                    type='password'
+                    value={formState.password}
+                    onChange={handleChange}
+                />
                 <button type='submit'>
                     Login
                 </button>
             </form>
+            {error && <div>Login failed</div>}
         </div>
     )
 }
